26c8521850c516a55b8ad2f171d2eb1d103f2f1c
王振 <rememberwz@gmail.com>
2017-12-23 12:49:37 +0800

    add plus

--- template/build/webpack.dev.web.conf.js
+++ template/build/webpack.dev.web.conf.js
@@ -5,6 +5,7 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
